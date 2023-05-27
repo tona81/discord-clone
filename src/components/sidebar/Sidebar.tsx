5aa4d3ad004c2497c7043508a8483dd560aababf
tona81 <t.nakashiro81@gmail.com>
2023-05-27 18:07:54 +0900

    sidebarHeader

---
+++ src/components/sidebar/Sidebar.tsx
@@ -0,0 +1,49 @@
+import React from "react";
+import "./Sidebar.scss";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import AddIcon from "@mui/icons-material/Add";
+import { Add } from "@mui/icons-material";
+import SidebarChannel from "./SidebarChannel";
+
+const Sidebar = () => {
+  return (
+    <div className="sidebar">
+      {/* sidebarLeft */}
+      <div className="sidebarLeft">
+        <div className="serverIcon">
+          <img src="./logo192.png" alt="" />
+        </div>
+        <div className="serverIcon">
+          <img src="./logo192.png" alt="" />
+        </div>
+      </div>
+      {/* sidebarRight */}
+      <div className="sidebarRight">
+        <div className="sidebarTop">
+          <h3>Discord</h3>
+          <ExpandMoreIcon />
+        </div>
+
+        {/* sidebarCHannels */}
+        <div className="sidebarChannels">
+          <div className="sidebarChannelsHeader">
+            <div className="sidebarHeader">
+              <ExpandMoreIcon />
+              <h4>プログラミングチャンネル</h4>
+            </div>
+            <AddIcon className="sidebarAddIcon" />
+          </div>
+
+          <div className="sidebarChannelList">
+            <SidebarChannel />
+            <SidebarChannel />
+            <SidebarChannel />
+            <SidebarChannel />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;

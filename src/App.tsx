94d939c733660b01c5bb3ea9f082d698dfbfa8f7
tona81 <t.nakashiro81@gmail.com>
2023-05-28 18:38:19 +0900

    chat

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import React from "react";
 import "./App.scss";
 import Sidebar from "./components/sidebar/Sidebar";
+import Chat from "./components/chat/Chat";
 
 function App() {
   return (
     <div className="App">
       {/* sidebar */}
       <Sidebar />
-      {/* home */}
+
+      {/* chat */}
+      <Chat />
     </div>
   );
 }
